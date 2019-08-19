f3812f58c558840082ae82b87581a6bd047b9351
Alex kagai <alexkagai@outlook.com>
2019-08-19 18:37:50 +0300

    The Following Commit: 1. Updates Tests for the project. 2. Enables Reusable templates. 3. Use Django widget tweaks to render forms.

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 
-from .models import Board
+from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 
 def home(request):
@@ -11,3 +12,43 @@ def home(request):
     }
 
     return render(request, 'home.html', context)
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    # TODO: get the currently logged in user
+    user = User.objects.first()
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message= form.cleaned_data.get('message'),
+                topic = topic,
+                created_by = user
+            )
+            # TODO: redirect to the created topic page
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+    # if request.method == 'POST':
+    #     form = NewTopicForm(request.POST)
+    #     if form.is_valid():
+    #         topic = form.save()
+    #         return redirect('board_topics', pk=board.pk)
+    # else:
+    #     form = NewTopicForm()
+    # return render(request, 'new_topic.html', {'form': form})
