cc67af857e71f7addee24c12d7c9d6eaad92e509
Alex Kagai <alexkagai@outlook.com>
2021-10-06 16:51:48 +0300

    Added a back link to board_topics page

--- boards/views.py
+++ boards/views.py
@@ -11,4 +11,4 @@ def board_topics(request, pk):
     """Show topics on board
     """
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'board.html', {'board': board})
+    return render(request, 'board_topics.html', {'board': board})
