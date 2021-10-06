aacac8d6e35ea75899c1e84a34a7b252ddd280e9
Alex Kagai <alexkagai@outlook.com>
2021-10-06 13:23:53 +0300

    Add Urls for Boards

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Board
 
-# Create your views here.
+def index(request):
+    boards = Board.objects.all()
+    board_names = []
+
+    for board in boards:
+        board_names.append(board.name)
+    
+    output = '<br>'.join(board_names)
+
+    return HttpResponse(output)
