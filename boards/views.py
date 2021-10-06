b6c147f053a2c5ecccd7e69a73833efd9a6062eb
Alex Kagai <alexkagai@outlook.com>
2021-10-06 14:18:03 +0300

    Add Tests & handle static files Style the boards display

--- boards/views.py
+++ boards/views.py
@@ -1,14 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Board
 
-def index(request):
+def home(request):
     boards = Board.objects.all()
-    board_names = []
-
-    for board in boards:
-        board_names.append(board.name)
     
-    output = '<br>'.join(board_names)
-
-    return HttpResponse(output)
+    context = {'boards': boards}
+    return render(request, 'home.html', context)
