75f0f05b2d50a5466ca99538e39e78f7c1f06ca0
Alex Kagai <alexkagai@outlook.com>
2021-10-06 12:41:45 +0300

    Add boards app models & migrations

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+# TODO: Board: name, description
+# TODO: Topic: subject, last_update, starter, board
+# TODO: Post: message, created_at, updated_at, created_by, updated_by
+# TODO: User: username, password, email, is_superuser
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
+    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
+
+
+class Posts(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='+')
