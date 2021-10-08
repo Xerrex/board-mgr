c4317a63dcf35c6fd7be7489f15695c9c4993ff7
Alex Kagai <alexkagai@outlook.com>
2021-10-08 09:10:07 +0300

    Added Email field to signup form

--- accounts/tests/test_signup.py
+++ accounts/tests/test_signup.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from django.contrib.auth.forms import UserCreationForm
+from  accounts.forms import SignupForm
 
 from accounts.views import signup
 
@@ -26,4 +26,14 @@ class SignupTests(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignupForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)   
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .forms import SignupForm
 
 
 def signup(request):
     """Handle user signing up operations
     """
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, "accounts/signup.html", {"form": form})
