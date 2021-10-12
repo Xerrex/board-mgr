d06b039cacc54c7f4b10e05f3bd85ff3c70950db
Alex Kagai <alexkagai@outlook.com>
2021-10-12 11:51:13 +0300

    Add the password reset Feature

---
+++ accounts/tests/test_passwordReset_invalid.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.core import mail
+
+
+class InvalidPasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.post(url, {'email': 'donotexist@email.com'})
+
+    def test_redirection(self):
+        '''
+        Even invalid emails in the database should
+        redirect the user to `password_reset_done` view
+        '''
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_no_reset_email_sent(self):
+        self.assertEqual(0, len(mail.outbox))
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
-# TODO:checkout Auth Views
-
-loginView = auth_views.LoginView.as_view(template_name='accounts/login.html')
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', loginView, name='login'),
+    path('logout/', views.logoutView, name='logout'),
+    path('login/', views.loginView, name='login'),
+    path('password-reset/', views.passwordResetView, name="password_reset"),
+    path('password-reset/done/', views.passwdResetDoneView, name="password_reset_done"),
+    path('password-reset/<uidb64>/<token>/', views.passwadResetConfirmView, name="password_reset_confirm"),
+    path('password-reset/complete/', views.passwordResetCompleteView, name="password_reset_complete"),
 ]
