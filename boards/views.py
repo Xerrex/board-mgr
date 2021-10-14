3b3b5cdb49927635df7228f3e14e8801cd41de6b
Alex Kagai <alexkagai@outlook.com>
2021-10-14 22:08:04 +0300

    Tweak pagination, last_update

--- boards/views.py
+++ boards/views.py
@@ -4,6 +4,7 @@ from django.db.models import Count
 from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
+from django.urls import reverse
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 
@@ -65,8 +66,11 @@ class PostListView(ListView):
     #TODO: Modify Post Design
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
     
@@ -85,7 +89,18 @@ def topic_reply(request, pk, topic_pk):
             post.topic = topic
             post.created_by = request.user
             post.save()
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+
+            topic.last_updated = timezone.now()
+            topic.save()
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'boards/topic_reply.html', {'topic': topic, 'form': form})
